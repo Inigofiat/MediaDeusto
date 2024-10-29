2fcf4be001ce66f7467a5646e23a9bed76b6ae14
Iñigo Fiat <inigo.fiat@opendeusto.es>
2024-10-29 13:32:54 +0100

    Update admin.py

--- proyectoMediaDeusto/appMediaDeusto/admin.py
+++ proyectoMediaDeusto/appMediaDeusto/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 
+from .models import Marca, Modelo, Dispositivo
+
+admin.site.register(Marca)
+admin.site.register(Dispositivo)
+admin.site.register(Modelo)
 
 # Register your models here.
