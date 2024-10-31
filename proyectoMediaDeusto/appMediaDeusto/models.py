7c07da2ef4659708f00cb58b2986dab0441e3d05
Iñigo Fiat <inigo.fiat@opendeusto.es>
2024-10-31 08:05:12 +0100

    Update models.py

--- proyectoMediaDeusto/appMediaDeusto/models.py
+++ proyectoMediaDeusto/appMediaDeusto/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Marca(models.Model):
     nombre = models.CharField(max_length=100)
-    id = models.IntegerField()
 
 def __str__(self):
     return self.nombre
